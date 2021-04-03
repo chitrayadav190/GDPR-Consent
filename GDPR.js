ec247497adc582bcd4e92983a403bf4e2e48b316
chitrayadav190 <64373447+chitrayadav190@users.noreply.github.com>
2021-04-03 12:30:08 +0200

    Add files via upload

---
+++ GDPR.js
@@ -0,0 +1,19 @@
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+},2000);
+
+function noscroll(){
+  window.scrollTo(0,0);
+}
+window.addEventListener("scroll",noscroll);
+
